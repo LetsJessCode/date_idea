5a079bc4677324825080f3b95c31fd6d6b6fe474
LetsJessCode <letsjesscode@gmail.com>
2020-10-19 15:16:06 -0400

    updated seed data

--- app/serializers/idea_serializer.rb
+++ app/serializers/idea_serializer.rb
@@ -1,4 +1,10 @@
 class IdeaSerializer < ActiveModel::Serializer
-  attributes :id, :category, :title, :how_to, :items
-  has_many :comments
+  attributes :id, :category, :title, :how_to, :items, :comments
+    has_many :comments
+  # def comments
+  #   array = []    
+  #   idea.object.comments.each{|comment| array.push({name: comment.name, comment: comment.comment, id: comment.id})}
+  #   array
+  # end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments
   resources :ideas
+  resources :comments
+  
+  resources :ideas do
+    resources :comments
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
